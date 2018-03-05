ca2b4cf84c613a37e7409c298c92f1e3648e2c62
SnackBoy2011 <snackboy2011@hotmail.com>
2018-03-05 15:08:58 -0500

    its ready

---
+++ Asignment1/Asignment1/Main.cpp
@@ -0,0 +1,10 @@
+#include "Engine.h"
+
+int main(int argc, char* argv[]) {
+
+	Engine eg;
+
+	eg.print();
+
+	return 0;
+}
---
+++ Asignment1/Core/Engine.cpp
@@ -0,0 +1,18 @@
+#include "Engine.h"
+#include <stdio.h>
+
+
+Engine::Engine()
+{
+}
+
+
+Engine::~Engine()
+{
+}
+
+void Engine::print() {
+	printf("Our game is being killed by artists");
+
+	getchar();
+}
